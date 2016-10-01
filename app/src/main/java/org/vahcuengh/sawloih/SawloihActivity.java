f2b08f79a74150a001ca33861c1be73e1967d7cb
thenlai <tl_huang@outlook.com>
2016-10-01 18:06:34 +0800

    init: project

---
+++ app/src/main/java/org/vahcuengh/sawloih/SawloihActivity.java
@@ -0,0 +1,13 @@
+package org.vahcuengh.sawloih;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SawloihActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sawloih);
+    }
+}
